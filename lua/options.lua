d6c9abdd6050b667006a0ed18f586a47e8fe14d4
Lasse <lamaxko@icloud.com>
2024-11-18 19:50:14 +0100

    Change of Lettering on open screen

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>")
 
 vim.cmd("set expandtab")
--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,61 +1,56 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"echasnovski/mini.icons", -- Optional for additional icons
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local ok_alpha, alpha = pcall(require, "alpha")
-		if not ok_alpha then
-			vim.notify("Failed to load alpha.nvim", vim.log.levels.ERROR)
-			return
-		end
+    "goolord/alpha-nvim",
+    dependencies = {
+        "echasnovski/mini.icons", -- Optional for additional icons
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local ok_alpha, alpha = pcall(require, "alpha")
+        if not ok_alpha then
+            vim.notify("Failed to load alpha.nvim", vim.log.levels.ERROR)
+            return
+        end
 
-		local ok_dashboard, dashboard = pcall(require, "alpha.themes.dashboard")
-		if not ok_dashboard then
-			vim.notify("Failed to load alpha dashboard theme", vim.log.levels.ERROR)
-			return
-		end
+        local ok_dashboard, dashboard = pcall(require, "alpha.themes.dashboard")
+        if not ok_dashboard then
+            vim.notify("Failed to load alpha dashboard theme", vim.log.levels.ERROR)
+            return
+        end
+        -- Custom Header
+        dashboard.section.header.val = {
+            [[      ___                                 ___     ]],
+            [[     /__/\         ___       ___         /__/\    ]],
+            [[     \  \:\       /__/\     /  /\       |  |::\   ]],
+            [[      \  \:\      \  \:\   /  /:/       |  |:|:\  ]],
+            [[  _____\__\:\      \  \:\ /__/::\     __|__|:|\:\ ]],
+            [[ /__/::::::::\ ___  \__\:\\__\/\:\__ /__/::::| \:\]],
+            [[ \  \:\~~\~~\//__/\ |  |:|   \  \:\/\\  \:\~~\__\/]],
+            [[  \  \:\  ~~~ \  \:\|  |:|    \__\::/ \  \:\      ]],
+            [[   \  \:\      \  \:\__|:|    /__/:/   \  \:\     ]],
+            [[    \  \:\      \__\::::/     \__\/     \  \:\    ]],
+            [[     \__\/          ~~~~                 \__\/    ]],
+        }
 
-		-- Custom Header
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
-
-		alpha.setup(dashboard.opts)
-		-- Custom Buttons
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+        alpha.setup(dashboard.opts)
+        -- Custom Buttons
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
             dashboard.button("g", "  Grep file", ":Telescope live_grep<CR>"),
-			dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
-			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/<CR>"),
-			dashboard.button("q", "  Quit", ":qa<CR>"),
-		}
+            dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+            dashboard.button("c", "  Configuration", ":e ~/.config/nvim/<CR>"),
+            dashboard.button("q", "  Quit", ":qa<CR>"),
+        }
 
-		-- Footer
-		dashboard.section.footer.val = {
-			"Neovim - Built for Power Users",
-		}
+        -- Footer
+        dashboard.section.footer.val = {
+            "Neovim - Built for Power Users",
+        }
 
-		-- Setup Alpha
-		alpha.setup(dashboard.opts)
+        -- Setup Alpha
+        alpha.setup(dashboard.opts)
 
-		-- Disable folding on the dashboard
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+        -- Disable folding on the dashboard
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
 }
